138d56561eb33ad13272b03cfa4e7283a00ba78d
Sellman <56926590+sSelmann@users.noreply.github.com>
2022-11-02 23:45:13 +0300

    getFileName method added to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import Utils.Events;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        List<String> fileNames=Events.getFileNames();
+
+        for (String s: fileNames) {
+            //TODO Events will be added here
+        }
 
     }
 
