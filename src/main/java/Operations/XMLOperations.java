9ecd6b66f995dbc02e671ff53966c8857506b66c
Sellman <56926590+sSelmann@users.noreply.github.com>
2022-11-16 20:26:16 +0300

    Added FigureData xml operations

--- src/main/java/Operations/XMLOperations.java
+++ src/main/java/Operations/XMLOperations.java
@@ -5,20 +5,39 @@ import com.jcabi.xml.XMLDocument;
 import jdk.internal.org.xml.sax.helpers.DefaultHandler;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class XMLOperations extends DefaultHandler {
 
     public static void main(String[] args) throws FileNotFoundException {
-        XML xml = new XMLDocument(new File("input/figuremap.xml"));
-        System.out.println(xml.xpath("//map//lib//part/@id"));
+        XML xml = new XMLDocument(new File("input/figuredata.xml"));
+        System.out.println(xml.xpath("//sets//set/@id"));
 
     }
 
     public static List<XML> getFiguremapLibs(String filePath) throws FileNotFoundException {
         XML xml = new XMLDocument(new File(filePath));
         return xml.nodes("//map//lib");
+    }
+
+    public static List<XML> getFigureDataSets(String filePath) throws FileNotFoundException {
+        XML xml = new XMLDocument(new File(filePath));
+        return xml.nodes("//sets//set");
+    }
+
+    public static String getFigureDataSetFigureType(String filePath, int index) throws FileNotFoundException {
+        List<XML> sets=getFigureDataSets(filePath);
+        String set=sets.get(index).toString();
+        XML xml = new XMLDocument(set);
+        System.out.println(set);
+        return xml.xpath("//set//part/@type").get(0);
+    }
+
+    public static List<String> getFigureDataSetIDs(String filePath) throws FileNotFoundException {
+        XML xml = new XMLDocument(new File(filePath));
+        return xml.xpath("//sets//set/@id");
     }
 
     public static List<String> getFiguremapLibIDs(String filePath) throws FileNotFoundException {
